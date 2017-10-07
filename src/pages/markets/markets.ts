9dedfbf64995052053f6a2bd7c85fa805e7093ec
S'Surakiti <ssurakiti@SSurakitis-MacBook-Pro.local>
2017-10-07 18:35:51 +0700

    change

--- src/pages/markets/markets.ts
+++ src/pages/markets/markets.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { GetApiCryptoProvider , cryptoNumbers ,cryto ,orderbook ,ask } from '../../providers/get-api-crypto/get-api-crypto';
+import { GetApiCryptoProvider , cryptoNumbers ,cryto ,orderbook ,asks , bids } from '../../providers/get-api-crypto/get-api-crypto';
 import _ from 'lodash';
 /**
  * Generated class for the MarketsPage page.
@@ -21,12 +21,12 @@ export class MarketsPage {
   constructor(public getCrypto :GetApiCryptoProvider,public navCtrl: NavController, public navParams: NavParams) {
   		this.getCrypto.loadBX().subscribe( data => { this.cryptoNumbers = Object.keys(data).map(key => data[key]) ;
     										console.dir(this.cryptoNumbers)},
-  										  error => {console.log("error: "+error);},
+  									  error => {console.log("error: "+error);},
   										  () => {console.log("Read park completely");})
   		// for(let i=0;i<27;i++){
   		// 	let number = this.cryptoNumbers[i];
   		// 	console.dir('Number :' + number);
-  		// }
+  		// } E-เตี้ยยยยยยยยยยยยยยยยยยยยยยยยย111
   }
 
   ionViewDidLoad() {
